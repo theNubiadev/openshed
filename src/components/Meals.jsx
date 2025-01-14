5b4b557f0eace20c2c37c69a9f0bafd2c152927c
Ajaiyeoba <ajaiyeobajibola@gmail.com>
2025-01-14 23:15:37 +0100

    added an array of objects prnted using map function and got images from unspplash

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -4,37 +4,42 @@ function Meals() {
         {
             name: "Original Fried Rice",
             description: "Classical, original & rich naija fried rice,. Search with choice of protein and sides.",
-            image: "",
+            image: "https://media.istockphoto.com/id/1127727388/photo/nigerian-jollof-rice-with-chicken-thigh.jpg?s=1024x1024&w=is&k=20&c=5Twu3vXlHUHzuJGyz2uyHMWsL0mRxV2P65FLeIlnl2Y=",
         },
         {
             name: "Original Fried Rice",
             description: "Classical, original & rich naija fried rice. Served with choice of protein and sides.",
-            image: "",
+            image: "https://media.istockphoto.com/id/2152921512/photo/kabsa-rice-with-chicken-saudi-arabian-dish-national-dish-homemade-no-people.jpg?s=1024x1024&w=is&k=20&c=mmzN-KOueghPKcYVmy7fvmC3hLkxXqdM0re_KqdGpLk=",
         },
         {
             name: "Chicken Suya Shawarma",
             description: "Our signature chicken suya wrapped in a warm pita bread with veg and special sauce served with sides of choice.",
-            image: "",
+            image: "https://media.istockphoto.com/id/1212447634/photo/nigerian-suya-chicken-roast-potatoes-and-butternut-squash-with-jollof.jpg?s=612x612&w=is&k=20&c=l53geedIWXEhLlscagzVt5FKpEIR3aUxgZT5sHGxvhk=",
         },
         {
             name: "Egusi Soup",
             description: "Thickened with ground melon seeds and typically served with pounded yam.",
-            image: "",
+            image: "https://media.istockphoto.com/id/1398351215/photo/egusi-soup-with-fufu-pounded-yam.jpg?s=612x612&w=is&k=20&c=0maKJoGaJz3qHNTdT79effGUiTdvq5pTDl2EBfz4-iI=",
         },
         {
             name: "Seafood Okro",
             description: "Loaded Okra with fresh fish, prawns, crab& Calamari. Served with choice of swallow",
-            image: ""
+            image: "https://media.istockphoto.com/id/1011036020/photo/thai-egyption-food.jpg?s=612x612&w=0&k=20&c=vWVDsUBk-odI5fyh8X8n2xsTLp1j8i3SptfAS73XV6U="
         },
         {
             name: "Authentic Abula",
             description: "Amala, Ewedu and Gbegiri with the different assorted meats. ",
-            image: "",
+            image: "https://media.istockphoto.com/id/1169415720/photo/regional-african-food.jpg?s=612x612&w=is&k=20&c=Of7Ijfl1FevgVs-YjXGGH_blZKdAcp_25JJCBbuF5yk=",
         },
         {
             name: "Loaded Efo Riro",
             description: "Vegetable loaded with chunks of beef, kpomo and fish. Served with choice of swallow",
-            image: "",
+            image: "https://media.istockphoto.com/id/2152921512/photo/kabsa-rice-with-chicken-saudi-arabian-dish-national-dish-homemade-no-people.jpg?s=1024x1024&w=is&k=20&c=mmzN-KOueghPKcYVmy7fvmC3hLkxXqdM0re_KqdGpLk=",
+        },
+        {
+            name: "Ewa Agoyin",
+            description: "Beans with assorted meat and fished garnished with fried sauce. Ready to go with bread or another topping. ",
+            image: "https://media.istockphoto.com/id/654775018/photo/a-plate-of-beans-served-with-red-palm-oil-stew-and-a-loaf-of-bread.jpg?s=612x612&w=is&k=20&c=Tpjxz0NnQ474d7QrFT9ZF6Q-U6sA_n0Nswc0eSTsDFU=",
         }
     ]
 
@@ -51,11 +56,12 @@ function Meals() {
                         <div key={index} className="bg-white rounded-lg overflow-hidden shadow-sm hover:shadow-md p-6">
                             <img src={meal.image} alt={meal.name}
                                 className="w-full h-48 object-cover rounded-sm"
-                          />
-                            <div>
+                            />
+                            <div className='mt-6'>
                                 <h2 className="text-xl font-semibold mb-2 ">{meal.name}</h2>
-                                <p className="text-base text-gray-600">{meal.description}</p>
+                                <p className="text-sm text-gray-600">{meal.description}</p>
                             </div>
+                            <button className=' text-orange-600 font-medium  mt-2 text-base '>Order Now </button>
                         </div>
                     ))}
                 </div>
