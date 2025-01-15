6a78f9bed467b269288b264da7cab1d1017f9a83
theNubiadev <ajaiyeobajibola@gmail.com>
2025-01-15 15:12:57 +0100

    created the route for other pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Home from '../src/pages/Home'
 import Navbar from '../src/components/Navbar'
 import Footer from '../src/components/Footer'
 import { Routes, Route } from 'react-router-dom'
+import Resturant from './pages/Resturant'
 function App() {
   return (
     <div className='min-h-screen bg-white'>
@@ -9,10 +10,8 @@ function App() {
       <Navbar />
 
       <Routes >
-
-
         <Route path='/' element={<Home />} />
-        
+        <Route path='/resturant' element={<Resturant />} />
       </Routes>
       <Footer />
     </div>
