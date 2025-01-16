e5b66891b136f962d8ba2aead052b64c4f6d73e1
theNubiadev <ajaiyeobajibola@gmail.com>
2025-01-16 12:16:19 +0100

    feat: completed the search feature with the ability to search the available resturants

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,15 @@
-import { useState } from "react"
+import PropTypes from 'prop-types'
 import { SearchIcon } from "lucide-react";
 
 
-function SearchBar() {
+function SearchBar({value, onChange}) {
 
-    const [value, OnChange] = useState('');
-    
   return (
     <div className="relative w-full max-w-md">
         <input 
             type="text"
             value={value}
-            onChange={(e) => OnChange(e.target.value)}
+            onChange={(e) => onChange(e.target.value)}
             placeholder="Search Resturants or Cusines"
             className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-600 focu:border-transparent"
         />
@@ -19,5 +17,9 @@ function SearchBar() {
     </div>
   )
 }
+SearchBar.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar
