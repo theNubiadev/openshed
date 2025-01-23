b5015fbcdedfc15b28dfa1664924981fc6300ef9
theNubiadev <ajaiyeobajibola@gmail.com>
2025-01-23 17:59:38 +0100

    now waiting

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// import MealContextProvider from './context/MealContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
