ec32ed04ad612857971ffa14d826f21600f0e113
theNubiadev <ajaiyeobajibola@gmail.com>
2025-02-01 10:03:17 +0100

    revamped

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
    const [isCartOpen, setIsCartOpen] = useState(false);
   //  const { cartItems } = MealContext();
   return (
-    <div>
+    <div> 
       <nav className="fixed w-full bg-white shadow-sm z-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
